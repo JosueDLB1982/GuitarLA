f5eeb8d60bb3da2dc122c2f42be0b0da617720fd
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-26 16:33:58 -0300

    El componente Iamge de Next.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
 import Link from "next/link"
+import Image from 'next/image'
 import styles from "../styles/Header.module.css"
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className="contenedor">
-                <div>
-
-                </div>
+                <Link href="/">
+                    <Image width={400} height={100} src="/img/logo.svg" alt="Imagen de logo" />
+                </Link>
 
                 <nav className={styles.navegacion}>
                     <Link href="/">Inicio</Link>
