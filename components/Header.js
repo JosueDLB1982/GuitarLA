28ac9c26f8e9554bfe11cebf4ba40a14f6a4905b
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-26 17:15:02 -0300

    Header terminado. Estilos aplicados con CSS

--- components/Header.js
+++ components/Header.js
@@ -6,16 +6,18 @@ const Header = () => {
     return (
         <header className={styles.header}>
             <div className="contenedor">
-                <Link href="/">
-                    <Image width={400} height={100} src="/img/logo.svg" alt="Imagen de logo" />
-                </Link>
+                <div className={styles.barra}>
+                    <Link href="/">
+                        <Image width={400} height={100} src="/img/logo.svg" alt="Imagen de logo" className={styles.logo}/>
+                    </Link>
 
-                <nav className={styles.navegacion}>
-                    <Link href="/">Inicio</Link>
-                    <Link href="/nosotros">Nosotros</Link>
-                    <Link href="/blog">Blog</Link>
-                    <Link href="/tienda">Tienda</Link>
-                </nav>
+                    <nav className={styles.navegacion}>
+                        <Link href="/">Inicio</Link>
+                        <Link href="/nosotros">Nosotros</Link>
+                        <Link href="/blog">Blog</Link>
+                        <Link href="/tienda">Tienda</Link>
+                    </nav>
+                </div>
             </div>
         </header>
     )
